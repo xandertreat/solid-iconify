dd7a8459af82efcad77644bc479399c22095cc6d
Xander Treat <xtreat@asu.edu>
2025-06-08 22:26:14 -0700

    patch
    this is the version with tsup that *works*

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,40 +7,45 @@ const generateSolidPresetOptions = (
 ): preset.PresetOptions => ({
 	entries: [
 		{
-			dev_entry: false,
-			server_entry: false,
+			dev_entry: true,
+			server_entry: true,
 			entry: "src/index.tsx",
 		},
 	],
-	cjs: false,
-	drop_console: !watching, // no console prints for prod.
+	cjs: true,
+	drop_console: !watching, // no console prints for prod
 });
 
 export default defineConfig((config) => {
-	const watching = Boolean(config.watch);
-	const solidPresetOptions = generateSolidPresetOptions(watching);
-	const parsedOptions = preset.parsePresetOptions(solidPresetOptions, watching);
+	const watch = !!config.watch;
+	const solidPresetOptions = generateSolidPresetOptions(watch);
+	const parsedOptions = preset.parsePresetOptions(solidPresetOptions, watch);
 
-	if (!watching) {
+	if (!watch) {
 		const packageFields = preset.generatePackageExports(parsedOptions);
 		preset.writePackageJson(packageFields);
 	}
 
-	const generated = preset
-		.generateTsupOptions(parsedOptions)
-		.map((buildCfg) => ({
-			...buildCfg,
-			name: pkg.name,
-			bundle: true,
-			minify: true,
-			minifyIdentifiers: true,
-			minifySyntax: true,
-			minifyWhitespace: true,
-			makeAbsoluteExternalsRelative: true,
-			treeshake: true,
-			shims: true,
-			clean: true,
-		}));
+	const generated = preset.generateTsupOptions(parsedOptions).map((cfg) => ({
+		...cfg,
+		tsconfig: "./tsconfig.json",
+		external: Object.keys(pkg.peerDependencies),
+		dts: !watch,
+		clean: !watch,
+		treeshake: true,
+		splitting: true,
+		platform: "neutral",
+		skipNodeModulesBundle: true,
+
+		minify: !watch,
+		minifyIdentifiers: !watch,
+		minifySyntax: !watch,
+		minifyWhitespace: !watch,
+		bundle: true,
+
+		shims: true,
+		cjsInterop: true,
+	}));
 
 	return generated;
 });
